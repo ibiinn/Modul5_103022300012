a58c6a3b79cfc30089be35161651044431753448
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:45:56 +0700

    commit message

--- Modul5_103022300012/Program.cs
+++ Modul5_103022300012/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Modul5_103022300012
+{
+    
+
+class Program
+{
+        class PemrosesData
+        {
+            public void DapatkanNilaiTerbesar<T>(T a, T b, T c)
+            {
+                dynamic temp1 = a;
+                dynamic temp2 = b;
+                dynamic temp3 = c;
+
+                if (temp1 > temp2 && temp1 > temp3)
+                {
+                    Console.WriteLine($"Nilai terbesar adalah: {a}");
+                }
+                else if (temp2 > temp1 && temp2 > temp3)
+                {
+                    Console.WriteLine($"Nilai terbesar adalah: {b}");
+                }
+                else
+                {
+                    Console.WriteLine($"Nilai terbesar adalah: {c}");
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            PemrosesData pemrosesData = new PemrosesData();
+            pemrosesData.DapatkanNilaiTerbesar(10.0, 30.0, 22.0);
+        }
+    }
+}
